1b7634990eefaaa591985fe119c5165e9d92f46d
Arthur Aukhatov <aaukhatov@gmail.com>
2017-02-04 20:25:27 +0500

    Remove unused classes

--- src/main/java/com/aukhatov/vaccinations/dao/VaccinationDao.java
+++
@@ -1,9 +0,0 @@
-package com.aukhatov.vaccinations.dao;
-
-import java.util.List;
-
-public interface VaccinationDao {
-    List<Vaccination> getAllVaccinationByPatient(long iian);
-    long addVaccination(Vaccination v);
-    void deleteVaccination(long id);
-}
