6303896d832d023a4848411fd1502b3019240332
Arthur Aukhatov <aaukhatov@gmail.com>
2017-02-05 20:57:15 +0500

    Fix instance name in SQL query

--- src/main/java/com/aukhatov/vaccinations/dao/repository/PatientRepository.java
+++ src/main/java/com/aukhatov/vaccinations/dao/repository/PatientRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
-    @Query("select p from patient p where p.iian = :iian")
+    @Query("select p from Patient p where p.iian = :iian")
     Patient findByIian(@Param("iian") String iian);
 
-    @Query("delete from patient p where p.iian = :iian")
+    @Query("delete from Patient p where p.iian = :iian")
     void deleteByIian(@Param("iian") String iian);
 }
